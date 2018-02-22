5874fa73fe12a75882dd4ab23b77e392dfca8d2a
FairFoxHanz <fairfoxhanz@gmail.com>
2018-02-22 13:33:47 +0100

    Added more sections

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const hbs = require("hbs");
+const fs = require("fs");
 
 const app = express();
+const port = process.env.PORT || 3001;
 
 hbs.registerPartials(`${__dirname}/views/partials`);
 app.set("view engine", "hbs");
 app.use(express.static(`${__dirname}/public`));
-hbs.registerHelper('getCurrentYear', () => {
-    return new Date().getFullYear();
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+});
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile("server.log", log + "\n", err => {
+    if (err) {
+      console.log("Unable to append to server.log");
+    }
+  });
+  next();
 });
 
 app.get("/", (req, res) => {
@@ -29,4 +44,4 @@ app.get("/bad", (req, res) => {
   });
 });
 
-app.listen(3001);
+app.listen(port);
